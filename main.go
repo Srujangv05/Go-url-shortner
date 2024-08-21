5fb3b1a7d07db4797ef78c54f2a10b2ff0b2fba8
Srujan G V <gvsrujan05@gmail.com>
2024-08-21 16:36:51 +0530

    Add: Redirect url

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func createURL(OriginalUrl string) string {
 		ShortUrl:     shortUrl,
 		CreationDate: time.Now(),
 	}
-	return "http://localhost:8080/" + shortUrl
+	return "http://localhost:3000/redirect/" + shortUrl
 }
 
 func getURL(id string) (URL, error) {
@@ -49,7 +49,6 @@ func getURL(id string) (URL, error) {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Request received")
 }
 
 func shortUrlHandler(w http.ResponseWriter, r *http.Request) {
@@ -63,13 +62,29 @@ func shortUrlHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	shortUrl := createURL(data.URL)
-	fmt.Fprintf(w, shortUrl)
+	response := struct {
+		ShortUrl string `json:"short_url"`
+	}{ShortUrl: shortUrl}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/redirect/"):]
+	url, err := getURL(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	http.Redirect(w, r, url.OriginalUrl, http.StatusMovedPermanently)
 }
 
 func main() {
 	// Register Handler
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/shorten", shortUrlHandler)
+	http.HandleFunc("/redirect/", redirectHandler)
 
 	// Start server
 	fmt.Println("Starting server on port 3000")
